6e057cff82ce251c3bfd8d2874545d5c10af3e5d
jwaes <jaco.waes@gmail.com>
2024-08-05 13:20:02 +0000

    df

--- static/src/js/action_manager.js
+++ static/src/js/action_manager.js
@@ -1,22 +1,21 @@
-/** @odoo-module */
-
+/** @odoo-module **/
 import { registry } from "@web/core/registry";
+import { BlockUI } from "@web/core/ui/block_ui";
 import { download } from "@web/core/network/download";
-import framework from 'web.framework';
-import session from 'web.session';
 
-
-registry.category("ir.actions.report handlers").add('xlsx', async (action) => {
-
+registry.category("ir.actions.report handlers").add("eauzon_xlsx", async function (action) {
     if (action.report_type === 'xlsx') {
-        framework.blockUI();
-        var def = $.Deferred();
-        var dd = session.get_file({
-            url: '/xlsx_reports',
-            data: action.data,
-            success: def.resolve.bind(def),
-            complete: framework.unblockUI,
-        });
-        return def;
-    }
+        BlockUI;
+	
+   await download({
+	
+           url: '/xlsx_reports',
+	
+           data: action.data,
+	
+           complete: () => unblockUI,
+	
+           error: (error) => self.call('crash_manager', 'rpc_error', error),
+	
+           });    }
 });
